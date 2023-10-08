1cba3fa38ef36a3950ea82ae2add34c363a5beb9
Aleksey Komov <lexxyar@ya.ru>
2023-10-08 17:56:29 +0300

    + ThemeToggle size

--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import {computed, onMounted, ref} from "vue";
+
+type TSize = 'sm' | 'md' | 'lg'
+const props = withDefaults(defineProps<{
+  size?: TSize,
+}>(), {
+  size: 'md'
+})
 
 const isDarkTheme = ref(false)
 const onToggleTheme = () => {
@@ -13,6 +20,26 @@ const onToggleTheme = () => {
     isDarkTheme.value = true
   }
 }
+
+onMounted(() => {
+  isDarkTheme.value = document.documentElement.classList.contains('dark')
+})
+
+const classSize = computed(() => {
+  let szIndex: string = '5'
+  switch (props.size) {
+    case 'sm':
+      szIndex = '4';
+      break;
+    case 'md':
+      szIndex = '5';
+      break;
+    case 'lg':
+      szIndex = '6';
+      break;
+  }
+  return `w-${szIndex} h-${szIndex}`
+})
 </script>
 
 <template>
@@ -21,11 +48,11 @@ const onToggleTheme = () => {
           @click="onToggleTheme"
   >
     <svg v-if="isDarkTheme"
-         class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+         :class="classSize" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
       <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
     </svg>
     <svg v-else
-         class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+         :class="classSize" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
       <path
           d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
           fill-rule="evenodd" clip-rule="evenodd"></path>
