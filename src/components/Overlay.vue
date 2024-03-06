a3d2870033ae51b5f1bcf963a2585a58a847b1b8
Aleksey Komov <lexxyar@ya.ru>
2024-03-06 12:15:28 +0300

    + dropdown menu anchors * overlay dark and light types * publish.yml for Node 20.x

--- src/components/Overlay.vue
+++ src/components/Overlay.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
 withDefaults(defineProps<{
   fixed?: boolean,
+  dark?: boolean,
 }>(), {
   fixed: false,
+  dark: true,
 })
 </script>
 
 <template>
   <!-- Overlay -->
-  <div class="bg-gray-900/50 dark:bg-gray-900/80 inset-0 z-30"
-       :class="(fixed?'fixed':'absolute')"
+  <div class="inset-0 z-30"
+       :class="{
+          'fixed':fixed,
+          'absolute':!fixed,
+          'bg-gray-900/50 dark:bg-gray-900/80': dark,
+          'bg-white/70 dark:bg-white/30':!dark,
+        }"
   >
     <slot/>
   </div>
